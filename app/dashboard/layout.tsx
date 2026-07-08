ddccbabbdcd79664626bd0891dd965cd1c0cc0db
muntasir.udoy.dev@gmail.com <muntasir.udoy.dev@gmail.com>
2026-07-08 17:31:36 +0600

    update

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -261,6 +261,7 @@ function DashboardLayoutContent({
           setSettingsInitialTab('personalizedAi')
           setShowSettingsModal(true)
         }}
+        //here
         onOpenBilling={() => {
           setSettingsInitialTab('billing')
           setShowSettingsModal(true)
